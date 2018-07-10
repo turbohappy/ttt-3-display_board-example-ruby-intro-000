e0a796e57e435d58043d5de6f603f6147aa65489
Brian Davis <turbohappy@gmail.com>
2018-07-10 21:07:25 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,13 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
 def display_board
-  state = Array.new(9, " ")
+  print_row
 
 end
 
-puts display_board
+def print_row
+  puts '-' * 11
+end
+
+
+display_board
